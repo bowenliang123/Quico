d7fe5fa721ba44bba44c3ef089f12471ba150b92
梁博文 <bowen.liang.123@gmail.com>
2016-03-10 22:58:29 +0800

    初始化了content_script 和backgroundscirpt,并完成了基本的消息通信试验。

---
+++ js/background.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('background.js');
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+    else
+      sendResponse({}); // snub them.
+  });
---
+++ js/content.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('content.js');
+
+console.log(window.location);
+
+//send reqeust to greet with background
+chrome.extension.sendRequest({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
+});
