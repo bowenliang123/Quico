739620f02caca3fbe114982993b43b2167627dce
梁博文 <lbw105516@alibaba-inc.com>
2016-03-11 12:37:26 +0800

    reformat code

--- js/background.js
+++ js/background.js
@@ -3,12 +3,12 @@
 console.log('background.js');
 
 chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-    else
-      sendResponse({}); // snub them.
-  });
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.greeting == "hello")
+            sendResponse({farewell: "goodbye"});
+        else
+            sendResponse({}); // snub them.
+    });
