ef9981394e76daae443678f1ae8b2677aa4e4c2d
梁博文 <lbw105516@alibaba-inc.com>
2016-03-12 09:39:37 +0800

    移除 onRequest 响应逻辑

--- js/background.js
+++ js/background.js
@@ -2,20 +2,6 @@
 
 console.log('background.js');
 
-chrome.extension.onRequest.addListener(
-    function (request, sender, sendResponse) {
-        console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting == "hello") {
-            sendResponse({farewell: "goodbye"});
-        }
-
-        else {
-            sendResponse({}); // snub them.
-        }
-    });
-
 
 var ports = [];
 //监听长时间连接
