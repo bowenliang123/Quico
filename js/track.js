720c2ea6652fb19955b057dbc831ce297a47208d
梁博文 <lbw105516@alibaba-inc.com>
2016-06-07 18:09:24 +0800

    v1.1.7    1. ga统计改为本地依赖，规避vpn下无法打开popup页的问题。2.选项页指向主面板页。3.改进主面板部分文本，中文化。

--- js/track.js
+++ js/track.js
@@ -25,7 +25,7 @@ _gaq.push(['_trackPageview']);
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    ga.src = chrome.extension.getURL('js/ga.js');
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 })();
