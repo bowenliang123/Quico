2b065077e08fa56dd3b7d41771373979d812f230
梁博文 <lbw105516@alibaba-inc.com>
2016-08-23 19:20:26 +0800

    优化主界面显示。优化二维码下载的文件名称。

--- js/util/qr_util.js
+++ js/util/qr_util.js
@@ -50,26 +50,37 @@ function invokeDownloadQrImgFile(url, base64img) {
     let image = new Image();
 
     image.onload = function () {
+        let canvasWidth = 600;
+        let canvasHeight = 280;
+        let qrCodePadding = 5;
+        let qrCodeMargin = 15;
+        let qrCodeLength = 250;
+        let lineWidth = 1;
+        let urlFontPxSize = 16;
+        let textX = qrCodeLength + qrCodeMargin * 2 + urlFontPxSize;
+        let textY = qrCodeMargin + urlFontPxSize;
+        let textLen = 30;
+        let textMaxWidth = (textLen - 2) * urlFontPxSize-10;
+
         //渲染背景颜色
         ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0, 0, 600, 280);
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        //渲染二维码外框
+        ctx.strokeStyle = "#a8a8a8";
+        ctx.lineWidth = lineWidth;
+        ctx.strokeRect(qrCodeMargin - qrCodePadding, qrCodeMargin - qrCodePadding, qrCodeLength + qrCodePadding * 2, qrCodeLength + qrCodePadding * 2);
 
         //渲染二维码
-        let imageMargin=15;
-        ctx.drawImage(image, imageMargin, imageMargin);
+        ctx.drawImage(image, qrCodeMargin, qrCodeMargin);
 
         //渲染网址
-        ctx.fillStyle = "#0066ff";      //文本颜色
-        let fontPxSize = 20;
-        ctx.font = fontPxSize + "px monospace";      //文本字体
-        let textX = 280;
-        let textY = imageMargin+fontPxSize;
-        let textMaxWidth = 320 - fontPxSize;
+        ctx.fillStyle = "#3333ff";      //文本颜色
+        ctx.font = urlFontPxSize + "px monospace";      //文本字体
         let index = 0;
-        let len = 25;
         while (index < url.length) {
-            let end = index + len;
-            let text = url.substr(index, len);
+            let end = index + textLen;
+            let text = url.substr(index, textLen);
             ctx.fillText(text, textX, textY, textMaxWidth);  //渲染网址
             textY = textY + 25;
             index = end;
@@ -82,7 +93,7 @@ function invokeDownloadQrImgFile(url, base64img) {
         downloadLink.href = canvas.toDataURL().replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 
         //文件名
-        downloadLink.download = `Quico ${url}.png`;
+        downloadLink.download = `Quico二维码 ${url}.png`;
 
         //加入到文档中
         document.body.appendChild(downloadLink);
